fad58754c33bee5cedf392ada6bbfd1b82fef8f9
Paolla Agard <paolla.agard@fiberwork.net>
2017-03-31 17:08:32 -0300

    Teste

---
+++ efigie/config.py
@@ -0,0 +1,9 @@
+DATABASES_NAME = 'd6qup9so6sb935'
+DATABASES_USER = 'tejyfgulfkdaiu'               
+DATABASES_PASSWORD = 'X8tzopqy2SJYQlQSBbol_A3rdL' 
+DATABASES_HOST = 'ec2-54-204-15-41.compute-1.amazonaws.com'
+
+DATABASES_LOCALHOST_NAME = 'efigie'
+DATABASES_LOCALHOST_USER = 'paolla'               
+DATABASES_LOCALHOST_PASSWORD = 'paolla'   
+DATABASES_LOCALHOST_HOST = 'localhost'     
