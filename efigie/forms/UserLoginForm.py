fab4b55fa64d8288feef9a475defada51727575d
Paolla Catherine <paollacath@gmail.com>
2017-04-05 20:38:14 -0300

    Update UserLoginForm.py
    Correção do placeholder

--- efigie/forms/UserLoginForm.py
+++ efigie/forms/UserLoginForm.py
@@ -7,7 +7,7 @@ from efigie.forms import *
 class UserLoginForm(forms.Form):
 
   username = forms.CharField(
-    widget=forms.TextInput(attrs={'placeholder':'Username'}))
+    widget=forms.TextInput(attrs={'placeholder':'Username or E-mail'}))
   
   password = forms.CharField(
     widget=forms.PasswordInput(attrs={'placeholder':'Password'}))
