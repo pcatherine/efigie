dc047eddd1a2580526cb856d39b3f1c3d0728f6e
Paolla Catherine <paollacath@gmail.com>
2017-04-21 16:49:36 -0300

    Key (refs #7)
    bug

--- efigie/models/Key.py
+++ efigie/models/Key.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 
 class Key(models.Model):
   user = models.ForeignKey(settings.AUTH_USER_MODEL)
