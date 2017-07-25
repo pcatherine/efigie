88361c9f66bbe9f88cd88e5dd32e928b1ea5b277
Paolla Catherine Agard <paollacath@gmail.com>
2017-07-25 16:41:16 -0300

    Added commands in manage.py

---
+++ efigie/management/commands/firststart.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+  help = "Executes manage's commands: startdatabase, makemigrations, makemigrations efigie, migrate and loaddata initial"
+  def handle(self, *args, **options):
+    os.system("python manage.py startdatabase")
+    os.system("python manage.py makemigrations")
+    os.system("python manage.py makemigrations efigie")
+    os.system("python manage.py migrate")
+    # os.system("python manage.py loaddata initial")
