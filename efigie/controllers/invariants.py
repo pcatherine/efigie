33fd0a46869cacde9d1836161fc77a3032af701f
Paolla Catherine Agard <paollacath@gmail.com>
2017-07-26 16:58:11 -0300

    Added controllers

---
+++ efigie/controllers/invariants.py
@@ -0,0 +1,19 @@
+from django.utils.translation import ugettext_lazy as _
+
+# Translators: This message appears on alert with %(model_name)s and %(item_name)s
+alert_add_success    = _("%s <b>%s</b> was successfully added.")
+alert_update_success = _("%s <b>%s</b> was successfully updated.")
+alert_delete_success = _("%s <b>%s</b> was successfully deleted.")
+
+alert_delete_error = _("%s <b>%s</b> could not be deleted.")
+alert_not_found_error = ('%s not found.')
+
+# Translators: This message appears on form with %(model_name)s and %(field_name)s
+form_already_exists = _("%s with this %s already exists.")
+
+# Translators: This message appears on button with %(model_name)s 
+button_update = _("Update %s")
+button_new = _("New %s")
+button_account_confirm = _('Confirm your account')
+button_set_password = _('Set Password')
+button_insert = _("Insert %s")
