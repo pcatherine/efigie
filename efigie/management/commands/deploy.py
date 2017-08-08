f02df442fa304e49890ceec3ad0aa42e4f91b026
Paolla Catherine Agard <paollacath@gmail.com>
2017-08-08 20:35:57 +0000

    teste

--- efigie/management/commands/deploy.py
+++ efigie/management/commands/deploy.py
@@ -9,11 +9,15 @@ from django.core.management.base import BaseCommand, CommandError
 class Command(BaseCommand):
   help = ""
   def handle(self, *args, **options):
+    os.system("rm efigie/config.py")
+    os.system('replace "efigie/config.py" "XYZ" -- .gitignore')
     os.system("travis login")
     os.system("travis encrypt-file efigie/config.py --add")
     os.system("git add .")
     os.system("git commit -m 'teste'")
     os.system("git push github")
+    os.system('replace "XYZ" "efigie/config.py"  -- .gitignore')
+
 
     # os.system("python manage.py makemigrations efigie")
     # os.system("python manage.py migrate")
