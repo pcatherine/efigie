1c0bc1076f48646020edb0e2d795bdec520ea25f
Paolla Catherine Agard <paollacath@gmail.com>
2017-08-09 16:13:58 -0300

    ...3

--- efigie/management/commands/deploy.py
+++ efigie/management/commands/deploy.py
@@ -10,14 +10,14 @@ class Command(BaseCommand):
   help = ""
   def handle(self, *args, **options):
 
-    os.system("travis login")
-    os.system("travis encrypt-file efigie/config.py --add")
+    # os.system("travis login")
+    # os.system("travis encrypt-file efigie/config.py --add")
     # os.system("rm efigie/config.py")
-    # os.system('replace "efigie/config.py" "XYZ" -- .gitignore')
+    os.system('replace "efigie/config.py" "XYZ" -- .gitignore')
     os.system("git add .")
-    os.system("git commit -m 'teste'")
-    os.system("git push github")
-    # os.system('replace "XYZ" "efigie/config.py"  -- .gitignore')
+    os.system("git commit -m 'deploy'")
+    os.system("git push heroku")
+    os.system('replace "XYZ" "efigie/config.py"  -- .gitignore')
 
 
     # os.system("python manage.py makemigrations efigie")
