0bef37627bcc402c18a020e985af1ea9d9beccbf
Paolla Catherine <paollacath@gmail.com>
2017-08-09 15:39:19 -0300

    ...

--- efigie/config.py
+++
@@ -1,9 +0,0 @@
-# Save as config.py
-
-DATABASES_NAME = 'd6qup9so6sb935'
-DATABASES_USER =  'tejyfgulfkdaiu'
-DATABASES_PASSWORD = 'X8tzopqy2SJYQlQSBbol_A3rdL'
-DATABASES_HOST = 'ec2-54-204-15-41.compute-1.amazonaws.com'
-
-EMAIL_HOST_USER = 'paollacath@gmail.com'
-EMAIL_HOST_PASSWORD = 'Paolla22'
